65783aaaf20f571c2f05599a4714a8eef51e81a5
Alexei <alexeim763@gmail.com>
2022-01-07 12:08:12 -0500

    Test to convert commonjs

--- index.ts
+++ index.ts
@@ -8,7 +8,7 @@
  * @typedef {ISO31661Entry['state']} State
  */
 
-export {iso31661} from './1.js'
+export {iso31661} from './1'
 export {iso31661Alpha2ToAlpha3} from './1-a2-to-1-a3.js'
 export {iso31661Alpha2ToNumeric} from './1-a2-to-1-n.js'
 export {iso31661Alpha3ToAlpha2} from './1-a3-to-1-a2.js'
